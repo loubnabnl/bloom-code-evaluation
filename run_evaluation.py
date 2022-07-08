11d09cfc96b5a194ebe6993e21ceb7ed82d5a003
loubnabnl <loubnabenallal1999@gmail.com>
2022-07-08 15:18:09 +0000

    update evaluation script to merge files in a single list

--- run_evaluation.py
+++ run_evaluation.py
@@ -2,9 +2,52 @@ import json
 import multiprocessing
 import os
 
-from arguments import HumanEvalArguments
 from datasets import  load_metric
 from transformers import HfArgumentParser
+
+from arguments import HumanEvalArguments
+
+def merge_generations(output_folder, num_tasks):
+    """merge the generations and references files of the tasks into single lists of all tasks"""
+    generations_exp1 = []
+    references_exp1 = []
+
+    generations_exp2 = []
+    references_exp2 = []
+
+    generations_exp3 = []
+    references_exp3 = []
+
+    #fill each list with the generations and references of each experiment
+    for task in range(1, num_tasks + 1):
+        task_path_exp1 = f"bloom/task_{task}_{task+1}/output_1"
+        task_path_exp2 = f"bloom/task_{task}_{task+1}/output_2"
+        task_path_exp3 = f"bloom/task_{task}_{task+1}/output_3"
+
+        with open(f"{task_path_exp1}/generations.json") as f:
+            # list of a list with 200 generations
+            gens = json.load(f)
+            generations_exp1.append(gens[0])
+        with open(f"{task_path_exp1}/references.json") as f:
+            # list with one test case inside
+            refs = json.load(f)
+            references_exp1.append(refs[0])
+
+        with open(f"{task_path_exp2}/generations.json") as f:
+            gens = json.load(f)
+            generations_exp2.append(gens[0])
+        with open(f"{task_path_exp2}/references.json") as f:
+            refs = json.load(f)
+            references_exp2.append(refs[0])
+                
+        with open(f"{task_path_exp3}/generations.json") as f:
+            gens = json.load(f)
+            generations_exp3.append(gens[0])
+        with open(f"{task_path_exp3}/references.json") as f:
+            refs = json.load(f)
+            references_exp3.append(refs[0])
+
+    return generations_exp1, references_exp1, generations_exp2, references_exp2, generations_exp3, references_exp3
 
 def main():
     # Setup configuration
@@ -29,21 +72,36 @@ def main():
         raise exception
 
     # Load generations and references
-    reference_file = f"{args.output_file}/references.json"
-    generation_file = f"{args.output_file}/generations.json"
-    with open(reference_file) as f:
-        references = json.load(f)
-    with open(generation_file) as f:
-        generations = json.load(f)
+    generations_exp1, references_exp1, generations_exp2, references_exp2, generations_exp3, references_exp3 = merge_generations(output_folder=args.output_file, num_tasks=args.num_tasks)
 
-    # Evaluate completions with "code_eval" metric
+    # Experiment 1
+    print("Evaluating completions for experiment 1 (temperature 0.2)")
     pass_at_k, _ = code_eval_metric.compute(
-        references=references, predictions=generations, num_workers=args.num_workers
+        references=references_exp1, predictions=generations_exp1, num_workers=args.num_workers
     )
-    print(f"Results: {pass_at_k}")
+    print(f"Results temperature 0.2: {pass_at_k}")
 
-    # Save results to json file
-    with open(f"{args.output_file}/eval_results.json", "w") as fp:
+    with open(f"{args.output_file}/eval_results_exp1.json", "w") as fp:
+        json.dump(pass_at_k, fp)
+
+    # Experiment 2
+    print("Evaluating completions for experiment 2 (temperature 0.6)")
+    pass_at_k, _ = code_eval_metric.compute(
+        references=references_exp2, predictions=generations_exp2, num_workers=args.num_workers
+    )
+    print(f"Results temperature 0.6: {pass_at_k}")
+
+    with open(f"{args.output_file}/eval_results_exp2.json", "w") as fp:
+        json.dump(pass_at_k, fp)
+
+    # Experiment 3
+    print("Evaluating completions for experiment 3 (temperature 0.8)")
+    pass_at_k, _ = code_eval_metric.compute(
+        references=references_exp3, predictions=generations_exp3, num_workers=args.num_workers
+    )
+    print(f"Results temperature 0.8: {pass_at_k}")
+
+    with open(f"{args.output_file}/eval_results_exp3.json", "w") as fp:
         json.dump(pass_at_k, fp)
 
 
